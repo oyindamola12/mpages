26776ca5e0ec95909d0c56e3584034c38d79177d
oyindamola12 <mshittu234@gmail.com>
2024-03-10 12:39:07 +0100

    new changes

--- public/listings.js
+++ public/listings.js
@@ -86,8 +86,7 @@ arrangeitems.href =`single-listing.html?businessName=${business.data.businessNam
 //  imgTag.src ='https://www.google.com/url?sa=i&url=https%3A%2F%2Ficonduck.com%2Ficons%2F251659%2Fprofile&psig=AOvVaw3ku22wSMS48htbmTL7nJO6&ust=1710111591417000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKjDzaak6IQDFQAAAAAdAAAAABAE'
 // }
       // let imageUrl = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Ficonduck.com%2Ficons%2F251659%2Fprofile&psig=AOvVaw3ku22wSMS48htbmTL7nJO6&ust=1710111591417000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKjDzaak6IQDFQAAAAAdAAAAABAE';
- imgTag.src =business.data.Images && business.data.Images.length > 0 ?business.data.Images[0]:'https://www.google.com/url?sa=i&url=https%3A%2F%2Ficonduck.com%2Ficons%2F251659%2Fprofile&psig=AOvVaw3ku22wSMS48htbmTL7nJO6&ust=1710111591417000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKjDzaak6IQDFQAAAAAdAAAAABAE';
-
+ imgTag.src =business.data.Images && business.data.Images.length > 0 ?business.data.Images[0]:'/Users/user/Downloads/mpagesWebsite/public/img/logo.png'
        // Assuming you have an 'imageUrl' property in your data
         imgTag.alt = 'Image'; // Provide alternative text for accessibility
         arrangepic.appendChild(imgTag);

0f86e048c142a7fb58090abd9c2fd9112a729f57
oyindamola12 <mshittu234@gmail.com>
2024-03-10 13:36:42 +0100

    new changes

--- public/listings.js
+++ public/listings.js
@@ -30,7 +30,7 @@ console.log(userUid)
 
 function navigateToUserProfile(businessId, businesslistingId) {
         // Redirect to the user profile page with the user ID as a query parameter
-        window.location.href = `/single-listing.html?id=${businessId}`;
+        window.location.href = `/single-listing.html?id=${businessId}&listingid=${ businesslistingId}`;
 
 }
 
@@ -93,7 +93,7 @@ arrangeitems.href =`single-listing.html?businessName=${business.data.businessNam
 //  imgTag.src ='https://www.google.com/url?sa=i&url=https%3A%2F%2Ficonduck.com%2Ficons%2F251659%2Fprofile&psig=AOvVaw3ku22wSMS48htbmTL7nJO6&ust=1710111591417000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKjDzaak6IQDFQAAAAAdAAAAABAE'
 // }
       // let imageUrl = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Ficonduck.com%2Ficons%2F251659%2Fprofile&psig=AOvVaw3ku22wSMS48htbmTL7nJO6&ust=1710111591417000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKjDzaak6IQDFQAAAAAdAAAAABAE';
- imgTag.src =business.data.Images && business.data.Images.length > 0 ?business.data.Images[0]:'img/logo.png'
+ imgTag.src =business.data.Images && business.data.Images.length > 0 ?business.data.Images[0]:'img/mPages Designs.png'
        // Assuming you have an 'imageUrl' property in your data
         imgTag.alt = 'Image'; // Provide alternative text for accessibility
         arrangepic.appendChild(imgTag);

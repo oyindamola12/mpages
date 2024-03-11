748296dd4b05ad8d02129c79704d1734a9f72e8f
oyindamola12 <mshittu234@gmail.com>
2024-03-12 00:41:00 +0100

    new changes

--- public/profile2.js
+++ public/profile2.js
@@ -34,7 +34,7 @@ var storedUserId =localStorage.getItem('selectedUserId');
 var userDataId =localStorage.getItem('userDataId');
 var storedUserData = JSON.parse(localStorage.getItem('selectedUserData'));
 var userUid =localStorage.getItem('userId');
-
+console.log(signedupAlready)
 // var donateBtn = document.getElementById('share2');
 // var divider= document.getElementById('divider');
 
@@ -573,4 +573,3 @@ function payWithPaystack(){
   }
   }
         // Check if the 'param' parameter exists and has a value
-console.log(signedupAlready)

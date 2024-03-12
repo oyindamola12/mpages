a70f0bb2456f92136e68455996db4b01d706f06d
oyindamola12 <mshittu234@gmail.com>
2024-03-12 03:47:57 +0100

    new changes

--- public/profile2.js
+++ public/profile2.js
@@ -165,9 +165,12 @@ fetch('/api/getSingleProfile', {
 
 
   //const imageUrl = userData.Images && userData.Images.length > 0 ?userData.Images[0]:userData.placeholder
-  const imageUrl ='img/mPages Designs.png'
-     element.setAttribute('data-setbg', 'img/mPages Designs.png');
+  const imageUrl = userData.Images[0]
+  if(userData.Images && userData.Images.length > 0){
+     element.setAttribute('data-setbg', imageUrl);
      element.style.backgroundImage = `url(${imageUrl})`;
+  }
+
      businessNameh2.textContent =  userData.businessName;
        timeToOpen.textContent =  userData.openingtime;
         timeToClose.textContent = userData.closingtime;

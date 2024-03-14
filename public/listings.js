3c9375560ca5c53214577c3f100da13db1f18ce2
oyindamola12 <mshittu234@gmail.com>
2024-03-14 15:59:41 +0100

    new changes

--- public/listings.js
+++ public/listings.js
@@ -1,9 +1,9 @@
 let map;
 let map2;
-let appendDiv = document.getElementById('col-lg-3');
+let appendDiv = document.getElementById('col-lg-5');
 let appendDiv2 = document.getElementById('col-lg-4');
-let appendDiv3 = document.getElementById('col-lg-5');
-let appendDiv4 = document.getElementById('col-lg-6');
+let appendDiv3 = document.getElementById('col-lg-');
+let appendDiv4 = document.getElementById('col-lg-');
 let container = document.getElementById("about-video");
 let myListings = document.getElementById('myListings');
 let inputIndustry =localStorage.getItem('industry');

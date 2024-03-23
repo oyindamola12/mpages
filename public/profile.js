e817a7eab64ea9c2d89b32510d85e03ac2f5c4a9
oyindamola12 <mshittu234@gmail.com>
2024-03-23 16:38:38 +0100

    new changes

--- public/profile.js
+++ public/profile.js
@@ -440,7 +440,7 @@ async function updateImage() {
     }
 }
 async function showEdit() {
-var editIcon = document.querySelectorAll(".fa-pencil")
+var editIcon = document.querySelectorAll(".my-selector")
  var shareSave = document.getElementById('shareSave');
   var shareEdit = document.getElementById('shareEdit');
     shareEdit.style.display='none'

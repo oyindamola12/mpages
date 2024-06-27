59c70787c83a2b13732ac451bc9ddbea64c52f6a
oyindamola12 <mshittu234@gmail.com>
2024-06-27 13:49:40 +0100

    some comments

--- public/listings.js
+++ public/listings.js
@@ -779,6 +779,7 @@ function getFiltered(industry, location) {
       loading.style.display = 'none';
 
       items.forEach((business) => {
+        console.log(business)
         const arrangeitems = document.createElement('a');
         arrangeitems.classList.add('arrange-items');
 

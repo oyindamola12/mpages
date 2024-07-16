90775c32344435c351143cbf2db4937323b9a46d
oyindamola12 <mshittu234@gmail.com>
2024-07-16 15:26:44 +0100

    first commit

--- public/admin.js
+++ public/admin.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const userTable = document.getElementById('userTable').getElementsByTagName('tbody')[0];
     const searchInput = document.getElementById('searchIndustryInput');
+    const location = document.getElementById("inputSuburb");
+    const userlength = document.getElementById("userlength");
 
     const fetchData = async () => {
         try {
             const response = await fetch('https://www.mpageshub.com/datalist');
             const users = await response.json();
-
+            userlength.textContent = users.length;
             users.forEach(user => {
                 const row = userTable.insertRow();
                 row.insertCell(0).textContent = user.businessName;
@@ -14,6 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 row.insertCell(2).textContent = user.email;
                 row.insertCell(3).textContent = user.phoneNo;
                 row.insertCell(4).textContent = user.businessAddress;
+
             });
         } catch (error) {
             console.error('Error fetching data:', error);
@@ -21,14 +24,14 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     searchInput.addEventListener('keyup', () => {
-        const filter = searchInput.value.toLowerCase();
+        const filter = searchInput.textContent.toLowerCase();
         const rows = userTable.getElementsByTagName('tr');
-
+        const getloc= location.value.toLowerCase().replace(/,/g, '').split(' ');
         Array.from(rows).forEach(row => {
             const industry = row.cells[1].textContent.toLowerCase();
             const businessAddress = row.cells[4].textContent.toLowerCase();
-            if (industry.includes(filter) || businessAddress.includes(filter)) {
-                row.style.display = '';
+            if (industry.includes(filter) && businessAddress.includes(getloc)) {
+                row.style.display = 'block';
             } else {
                 row.style.display = 'none';
             }

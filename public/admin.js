c039fcf08f54e7655c7bc1a6528b16ead2eb95b5
oyindamola12 <mshittu234@gmail.com>
2024-07-18 01:06:29 +0100

    first commit

--- public/admin.js
+++ public/admin.js
@@ -1,47 +1,41 @@
 const userTable = document.getElementById('userTable').getElementsByTagName('tbody')[0];
-    const searchInput = document.getElementById('searchIndustryInput');
-    const location = document.getElementById("inputSuburb");
-    const userlength = document.getElementById("userlength");
+        const searchInput = document.getElementById('searchIndustryInput');
+        const locationInput = document.getElementById("inputSuburb");
+        const userlength = document.getElementById("userlength");
 
+        document.addEventListener('DOMContentLoaded', () => {
+            const fetchData = async () => {
+                try {
+                    const response = await fetch('https://www.mpageshub.com/datalist');
+                    const users = await response.json();
+                    userlength.textContent = users.length;
+                    users.forEach(user => {
+                        const row = userTable.insertRow();
+                        row.insertCell(0).textContent = user.businessName;
+                        row.insertCell(1).textContent = user.industry;
+                        row.insertCell(2).textContent = user.email;
+                        row.insertCell(3).textContent = user.phoneNo;
+                        row.insertCell(4).textContent = user.businessAddress;
+                    });
+                } catch (error) {
+                    console.error('Error fetching data:', error);
+                }
+            };
 
-
-document.addEventListener('DOMContentLoaded', () => {
- const fetchData = async () => {
-        try {
-            const response = await fetch('https://www.mpageshub.com/datalist');
-            const users = await response.json();
-            userlength.textContent = users.length;
-            users.forEach(user => {
-                const row = userTable.insertRow();
-                row.insertCell(0).textContent = user.businessName;
-                row.insertCell(1).textContent = user.industry;
-                row.insertCell(2).textContent = user.email;
-                row.insertCell(3).textContent = user.phoneNo;
-                row.insertCell(4).textContent = user.businessAddress;
-
-            });
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    };
-
-
-
-    fetchData();
-});
-
-
-const filterData =()=>{
-    const filter = searchInput.textContent.toLowerCase();
-        const rows = userTable.getElementsByTagName('tr');
-        const getloc= location.value.toLowerCase().replace(/,/g, '').split(' ');
-        Array.from(rows).forEach(row => {
-            const industry = row.cells[1].textContent.toLowerCase();
-            const businessAddress = row.cells[4].textContent.toLowerCase();
-            if (industry.includes(filter) && businessAddress.includes(getloc)) {
-                row.style.display = 'block';
-            } else {
-                row.style.display = 'none';
-            }
+            fetchData();
         });
-}
+
+        const filterData = () => {
+            const filter = searchInput.textContent.toLowerCase();
+            const location = locationInput.value.toLowerCase();
+            const rows = userTable.getElementsByTagName('tr');
+            Array.from(rows).forEach(row => {
+                const industry = row.cells[1].textContent.toLowerCase();
+                const businessAddress = row.cells[4].textContent.toLowerCase();
+                if (industry.includes(filter) && businessAddress.includes(location)) {
+                    row.style.display = '';
+                } else {
+                    row.style.display = 'none';
+                }
+            });
+        }
